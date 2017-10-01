b0f91a963154b0c7326f08025ec08aebd14a0bdd
ishancoder <isaxena171@gmail.com>
2017-10-01 22:02:26 +0530

    add index.html and sample.env

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 const express = require('express');
 const pg = require('pg');
+const path = require('path');
+require('dotenv').config();
 
-const connectionString = process.env.DATABASE_URL || "postgres://edmynqjkishnfc:f6443300c7f634d81e6ee39522825d2cc191d5c0f2250de7360a7e9fb760f5d4@ec2-54-227-252-202.compute-1.amazonaws.com:5432/d8mjgv9gqsh3lr";
+const connectionString = process.env.DATABASE_URL;
 
 let client = new pg.Client(connectionString);
 client.connect();
 
 let app = express();
 
+app.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
 app.get("/branches/:ifsc", function(req, res, next) {
-    const id = req.params['ifsc'];
-    console.log("Incoming request for : " + id);
+    const ifsc = req.params['ifsc'];
+    console.log("Incoming request for : " + ifsc);
     
-    let query = client.query("SELECT ifsc, name AS bankName, branch, address, city, district, state FROM BRANCHES LEFT JOIN BANKS ON BRANCHES.bank_id = BANKS.id WHERE BRANCHES.ifsc = $1", [id], function(err, result) {
+    let query = client.query("SELECT ifsc, name AS bankName, branch, address, city, district, state FROM BRANCHES LEFT JOIN BANKS ON BRANCHES.bank_id = BANKS.id WHERE BRANCHES.ifsc = $1", [ifsc], function(err, result) {
         if(err) {
             return next(err);
+        }
+        if(result.rows.length === 0) {
+            return res.json("{\"error\" : \"Invalid IFSC : " + ifsc + "\"}")
         }
         res.json(JSON.stringify(result.rows[0]));
     });
 });
 
 app.get("/branches/:bank/:city", function(req, res, next) {
-    const bank = req.params['bank'];
-    const city = req.params['city'];
+    const bank = req.params['bank'].toUpperCase();
+    const city = req.params['city'].toUpperCase();
 
     let query = client.query("SELECT ifsc, name AS bankName, branch, address, city, district, state FROM BRANCHES LEFT JOIN BANKS ON BRANCHES.bank_id = BANKS.id WHERE BANKS.name = $1 AND BRANCHES.city = $2", [bank, city], function(err, result) {
         if(err) {
             return next(err);
+        }
+        if(result.rows.length === 0) {
+            return res.json("{\"error\": \"Invalid Input bank : "+ bank + ", city : " + city + "\"}")
         }
         res.json(JSON.stringify(result.rows));
     });
